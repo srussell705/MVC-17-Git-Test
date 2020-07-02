477f7fda1dd8c5c1df648c896a8960a2877172f0
Stephen Russell <stephen.russell@ringcontainer.com>
2020-07-02 18:47:50 -0500

    Updated a lot of code

---
+++ MVC 17 Git Test/Areas/Common/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_17_Git_Test.Areas.Common.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Common/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC 17 Git Test/Areas/User/Controllers/URLController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_17_Git_Test.Areas.User.Controllers
+{
+    public class URLController : Controller
+    {
+        // GET: User/URL
+        public ActionResult Index()
+        {
+            LinkHubDbEntities db = new LinkHubDbEntities();
+            ViewBag.CategoryID = new SelectList(db.tbl_Category, "CategoryId", "CategoryName");
+            return View();
+        }
+
+        public ActionResult Create(tbl_Url objUrl)
+        {
+            //LinkHubDbEntities db = new LinkHubDbEntities();
+            //ViewBag.CategoryID = new SelectList(db.tbl_Category, "CategoryId", "CategoryName");
+            return View();
+        }
+    }
+}

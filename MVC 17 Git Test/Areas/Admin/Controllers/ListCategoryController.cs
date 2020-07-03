74aba4eb6be4e32461ea7f971c342c409b8e5d88
Stephen Russell <stephen.russell@ringcontainer.com>
2020-07-02 19:27:17 -0500

    Updated to hold all controlers

---
+++ MVC 17 Git Test/Areas/Admin/Controllers/ListCategoryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_17_Git_Test.Areas.Admin.Controllers
+{
+    public class ListCategoryController : Controller
+    {
+        // GET: Admin/ListCategory
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC 17 Git Test/Areas/Common/Controllers/BrowseURLController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_17_Git_Test.Areas.Common.Controllers
+{
+    public class BrowseURLController : Controller
+    {
+        // GET: Common/BrowseURL
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
